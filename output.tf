65618116322a73d87aab20dcbfbb971e3ebbf983
sargeene <sargeene@gmail.com>
2024-08-03 16:39:46 +0100

    cloud-init.yml script updated

--- output.tf
+++ output.tf
@@ -4,6 +4,6 @@ output "public_ip" {
 
 # ssh -i ~/.ssh/azure_terraform_key Eka@51.145.36.212
 # ssh-keygen -t rsa -b 4096 -f  ~/.ssh/azure_terraform_key -C "vitaliseaka@ymail.com"
-# ssh -i ~/.ssh/azure_terraform_key Eka@51.143.159.249
+# ssh -i ~/.ssh/azure_terraform_key Eka@51.145.40.248
 
 # ssh-keygen -t rsa -b 4096 -f  ~/.ssh/azure_terraform_key -C "vitaliseaka@ymail.com"
