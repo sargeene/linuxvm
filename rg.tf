89e16df5779944a3a2cd3ef23810b0acd370f276
Rasheed <100427027+sargeene@users.noreply.github.com>
2025-01-10 21:52:52 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "this_rg" {
-  name     = "${local.owner}-${local.environment}-${var.rg}"
+  name     = "${local.owner}-${local.environment}-${var.rg_name}"
   location = "uksouth"
 }
